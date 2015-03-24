c559a2ad808d70b8953b0188fb286d1a8a4eb025
John Gerlock <john.gerlock@gmail.com>
2015-03-23 22:20:28 -0400

    Working excapt indexes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
 import logging
-
+FORMAT = "[%(levelname)s %(asctime)s %(filename)s:%(lineno)s] %(funcName)20s()  %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+logger = logging.getLogger(__name__)
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import WSGIApplication
@@ -8,48 +9,35 @@ from google.appengine.ext.webapp import Route
 from google.appengine.ext.webapp import RedirectHandler
 
 from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.api import users
 from google.appengine.api import memcache
-from google.appengine.ext import ndb
 
 from models import Link
 
 
-class Index(webapp.RequestHandler):
-
-    def get(self):
-        self.response.write('hello')
-
+class Create(webapp.RequestHandler):
     def post(self):
 #        if not users.is_current_user_admin():
 #            self.abort(405)
-
-        fields = ('target_url', 'short_path', 'utm_campaign', 'utm_source', 'utm_medium',
-                  'utm_content')
+        fields = ('target_url', 'path', 'utm_campaign', 'utm_source',
+                    'utm_medium', 'utm_content')
         data = {k: self.request.get(k) for k in fields}
-        data['user'] = users.get_current_user()
-
-        for attemps in xrange(3):
-            link = Link.new(**data).get()
-            if link:
-                break
-        else:
-            self.abort(400)
-
-        memcache.set(link.url, link.target_url)
-
-        self.response.write(link.json)
+        link = Link.create(**data).get()
+        return self.response.write(link.json) if link else self.abort(400)
 
 
-def get_redirect_url(handler, *args, **kwargs):
-    link = handler.request.application_url
-    url = memcache.get(link) or Link.get_by_id(link)
-    return url or handler.abort(404)
+def redirector(handler, *args, **kwargs):
+    link = handler.request.path_url
+    logger.debug('redirector link:%s', link)
+    url = str(memcache.get(link)) or str(Link.get_by_id(link).target_url)
+    logger.debug('redirector target_url:%s', url)
+    return url
 
 
 app = WSGIApplication([
-    Route('/', Index),
-    Route('/(.+)', RedirectHandler, defaults={'_uri': get_redirect_url})
+    Route('/', Create, methods=['POST']),
+    Route('/<path>', RedirectHandler, methods=['GET'], defaults={'_uri': redirector}),
+    Route('/', RedirectHandler, methods=['GET'],
+          defaults={'_uri':'http://detroitartmap.com'}),
 ], debug=True)
 
 
--- models.py
+++ models.py
@@ -10,16 +10,23 @@
 from google.appengine.ext import ndb
 from google.appengine.api import memcache
 from google.appengine.api import users
+from google.appengine.api import app_identity
 
 import logging
+FORMAT = "[%(levelname)s %(asctime)s %(filename)s:%(lineno)s] %(funcName)20s()  %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+logger = logging.getLogger(__name__)
+
 import urlparse
 import urllib
 
-#from properties import URLProperty
 import short_url
 
-DEFAULT_SHORT_SCHEME = 'http'
-DEFAULT_SHORT_NETLOC = '4RT.mp'
+DEFAULT_SCHEME = 'http'
+
+DEFAULT_NETLOC = '4RT.mp'
+if app_identity.get_default_version_hostname().startswith('localhost'):
+    DEFAULT_NETLOC = app_identity.get_default_version_hostname()
 
 
 class View(ndb.Model):
@@ -27,23 +34,24 @@ class View(ndb.Model):
     create_date = ndb.DateTimeProperty(auto_now_add=True)
 
 
-class User(ndb.Model):
-    mixpanel_distinct_id = ndb.TextProperty()
-    google_analytics_client_id = ndb.TextProperty()
-    mailchimp_user_id = ndb.TextProperty()
-    artmap_user_id = ndb.TextProperty()
-    create_date = ndb.DateTimeProperty(auto_now_add=True)
-
-
 class Link(ndb.Model):
-    target_url = ndb.TextProperty(required=True)
+    target_url = ndb.TextProperty(required=True, default='oops')
+    sequence_index = ndb.ComputedProperty(lambda self: self.index)
+    create_date = ndb.DateTimeProperty(auto_now_add=True)
+    created_by = ndb.UserProperty(
+        validator=lambda p,
+        v: users.get_current_user())
+    vanity_path = ndb.BooleanProperty(default=False)
+
+    artmap_user_id = ndb.TextProperty()
+    mixpanel_distinct_id = ndb.TextProperty()
+    mailchimp_user_id = ndb.TextProperty()
+    google_analytics_client_id = ndb.TextProperty()
+
     utm_campaign = ndb.TextProperty()
     utm_source = ndb.TextProperty()
     utm_medium = ndb.TextProperty()
     utm_content = ndb.TextProperty()
-    create_date = ndb.DateTimeProperty(auto_now_add=True)
-    user = ndb.UserProperty(validator=lambda p, v: users.get_current_user())
-    vanity_path = ndb.BooleanProperty(default=False)
 
     def __repr__(self):
         return 'link:%s target:%s' % (self.key.id(), self.target_url)
@@ -56,47 +64,107 @@ class Link(ndb.Model):
 
     @staticmethod
     def update_query_string(target_url, **kwargs):
+        logger.debug('update_query_string target_url:%s, kwargs:%s',
+                     target_url, kwargs)
+
         parts = urlparse.urlsplit(target_url)
+        logger.debug('update_query_string parts:%s', parts)
         query_dict = urlparse.parse_qs(parts[3])
+        logger.debug('update_query_string query_dict:%s', query_dict)
         query_dict.update(kwargs)
+        logger.debug('update_query_string updated query_dict:%s', query_dict)
+
         query_string = urllib.urlencode(query_dict)
-        return urlparse.urlunsplit(parts[0], parts[1],
-                                   parts[2], query_string, parts[4])
+        logger.debug('update_query_string query_string:%s', query_string)
+        url = urlparse.urlunsplit(parts[0], parts[1],
+                                  parts[2], query_string, parts[4])
+        logger.debug('update_query_string new url:%s', url)
+        return url
 
     @classmethod
-    def compute_shortpath(cls):
+    def get_highest_index(cls):
+        try:
+            return int(cls.query().order(-cls.sequence_index).get().sequence_index)
+        except Exception as e:
+            logger.error(e)
+            return 1
 
     @classmethod
-    def new(cls, target_url, short_scheme=DEFAULT_SHORT_SCHEME,
-            short_netloc=DEFAULT_SHORT_NETLOC, short_path=None,
-            utm_campaign=None, utm_source=None, utm_medium=None,
-            utm_content=None):
+    def create_key(cls, scheme, netloc, path):
+        logger.debug(
+            'create_key scheme:%s, netloc:%s, path:%s',
+            scheme,
+            netloc,
+            path)
+        if not path:
+            logger.debug('create_key no vanity path-> generating unique_path')
+            count = cls.get_highest_index()
+            logger.debug('create_key entity count: %s', count)
+            path = short_url.encode_url(count)
+            logger.debug('create_key encoded entity count: %s', path)
+        logger.debug('create_key path:%s', path)
+        url = urlparse.urlunsplit([scheme, netloc, path, '', ''])
+        logger.debug('create_key url:%s', url)
+        entity = cls(key=ndb.Key(cls, url))
+        result = ndb.transaction(
+            lambda: entity.put() if not entity.key.get() else None)
+        logger.debug('create_key result:%s', result)
+        if result:
+            return result
+        else:
+            raise KeyError('Link already exists for url:%s' % url)
+
+    @classmethod
+    def create(cls, target_url=None, scheme=DEFAULT_SCHEME,
+               netloc=DEFAULT_NETLOC, path=None,
+               utm_campaign=None, utm_source=None, utm_medium=None,
+               utm_content=None):
 
         if any([utm_campaign, utm_source, utm_medium, utm_content]):
             target_url = cls.update_query_string(target_url,
                                                  utm_campaign=utm_campaign,
                                                  utm_source=utm_source,
                                                  utm_medium=utm_medium,
-
                                                  utm_content=utm_content)
-        if not short_path:
-            short_path = cls.compute_shortpath()
-            vanity_path = False
-        else:
-            vanity_path = True
-        path = urlparse.urlunsplit([short_scheme, short_netloc,
-                                    short_path, '',''])
-        if ndb.Key(cls,path).get():
-            raise KeyError
 
-        link = cls(id=path, target_url=target_url, utm_campaign=utm_campaign,
+        vanity_path = False if not path else True
+        logger.debug('create vanity_path: %s', vanity_path)
+        key = cls.create_key(scheme, netloc, path)
+        logger.debug('create key: %s', key)
+        link = cls(key=key, target_url=target_url, utm_campaign=utm_campaign,
                    utm_source=utm_source, utm_medium=utm_medium,
-                   utm_content=utm_content, user=user, vanity_path=vanity_path)
-        return link.put()
+                   utm_content=utm_content, vanity_path=vanity_path)
+        logger.debug('create link: %s', link)
+        key = link.put()
+        if key:
+            memcache.set(key=link.key.id(), value=link.target_url)
+            logger.debug(
+                'Added to memcache: %s=%s',
+                link.key.id(),
+                link.target_url)
+
+        logger.debug('create key: %s', key)
+        return key
 
     @property
     def path(self):
-        return urlparse.urlsplit(self.key.id())
+        path = urlparse.urlsplit(self.key.id())[3]
+        logger.debug('path: %s', path)
+        return path
+
+
+    @property
+    def index(self, path=None):
+        if self.vanity_path is True:
+            logger.debug('index vanity path: %s index = 0', self)
+            return 0
+        try:
+            index = short_url.decode_url(self.path)
+            logger.debug('index for %s = %s', self, index)
+            return index
+        except Exception as e:
+            logger.error(e)
+            return 1
 
     @property
     def url(self):
